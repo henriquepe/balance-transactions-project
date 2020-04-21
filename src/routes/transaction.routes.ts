dc2d1d23f7f8bdc7e4f0057b783243161e073f6f
Henrique <henrique.p.espindola@gmail.com>
2020-04-20 22:56:30 -0300

    added routes

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,23 +1,37 @@
 import { Router } from 'express';
-
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
-
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
-  try {
-    // TODO
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  // TODO
+  const transactions = transactionsRepository.all();
+  const balance = transactionsRepository.getBalance();
+
+  const list = {
+    transactions,
+    balance,
+  };
+
+  return response.json(list);
 });
 
 transactionRouter.post('/', (request, response) => {
   try {
     // TODO
+    const { title, value, type } = request.body;
+
+    const newTransaction = new CreateTransactionService(transactionsRepository);
+
+    const transaction = newTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }

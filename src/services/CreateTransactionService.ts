bb731468a7a2523a8380019c41cb54884c07a7c7
Henrique <henrique.p.espindola@gmail.com>
2020-04-20 22:56:55 -0300

    added service

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, value, type }: Request): Transaction {
     // TODO
+    const balance = this.transactionsRepository.getBalance();
+
+    const verify = type === 'outcome' && value > balance.total;
+
+    if (verify) {
+      throw Error(
+        'The cashier was be extrapolated, cant create a new output transaction',
+      );
+    }
+    const newTransaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return newTransaction;
   }
 }
 
